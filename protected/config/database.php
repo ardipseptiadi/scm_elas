31f604c38d89a8a60a8e5bc90920264b7d35b482
ardi <ardi@docotel.co.id>
2019-02-26 23:11:38 +0700

    update gitignore

--- protected/config/database.php
+++ protected/config/database.php
@@ -1,14 +1,16 @@
 <?php
 
 // This is the database connection configuration.
+$ini = @parse_ini_file('env/.env', true);
 return array(
 	// 'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
 
-	'connectionString' => 'mysql:host=localhost:3308;dbname=scm_elas',
+	'connectionString' => isset($ini['database']['conn_str'])
+                ? $ini['database']['conn_str']:"mysql:host=localhost;dbname=scm_elas",
 	'emulatePrepare' => true,
-	'username' => 'root',
-	'password' => 'abc123',
+	'username' => isset($ini['database']['user']) ? $ini['database']['user'] :'root'
+	'password' => isset($ini['database']['password']) ? $ini['database']['password'] :'',
 	'charset' => 'utf8',
 
 );
